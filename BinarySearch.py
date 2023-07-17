c75480ad9cc0625ad130a1212fc71a56c5124b0a
Md Tariq Hasan Siddique <114738441+tariq005@users.noreply.github.com>
2023-07-17 22:06:25 +0600

    Add files via upload

---
+++ BinarySearch.py
@@ -0,0 +1,19 @@
+n = int(input())
+l, r = 0, n-1
+a = list(range(1, n+1))
+print(a)
+x = int(input())
+
+while l < r:
+    m = (l+r)//2
+    y = a[m]
+
+    if y<x:
+        l = m+1
+    elif y>x:
+        r = m
+    else:
+        print(m)
+        break
+else:
+    print(-1)
