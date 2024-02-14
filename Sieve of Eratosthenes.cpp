0040c01a4565c425baccf589435f8185aab91dc2
Md Tariq Hasan Siddique <114738441+tariq005@users.noreply.github.com>
2024-02-15 00:15:56 +0600

    Add files via upload

---
+++ Sieve of Eratosthenes.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<bool> is_prime(n+1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= n; i++)
+    {
+        if (is_prime[i] && (long long)i * i <= n)
+        {
+            for (int j = i * i; j <= n; j += i)
+                is_prime[j] = false;
+        }
+    }
+
+    for (int i = 2; i <= n; i++)
+        if (is_prime[i])
+        cout << i << endl;
+    return 0;
+}
